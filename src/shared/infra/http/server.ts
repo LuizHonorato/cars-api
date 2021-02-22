24969e89906972ec36d47886a38c22c797ee14be
LuizHonorato <lhehonorato@gmail.com>
2021-02-22 20:40:56 -0300

    typeorm and mongodb setup.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+
+import '@shared/infra/typeorm';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(3333, () => {
   console.log('Cars API started on port 3333! 🚀️');
