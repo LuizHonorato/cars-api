b2e068315bb1146d4c213ef05c9850d30622c9f4
LuizHonorato <lhehonorato@gmail.com>
2021-02-22 22:20:14 -0300

    creating car schema.

---
+++ src/modules/cars/infra/typeorm/schemas/Car.ts
@@ -0,0 +1,39 @@
+import { Column, Entity, ObjectID, ObjectIdColumn } from 'typeorm';
+
+export enum Exchange {
+  MANUAL = 'MANUAL',
+  AUTOMATICO = 'AUTOMÁTICO',
+}
+
+@Entity('cars')
+class Car {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column()
+  brand: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  version: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  mileage: number;
+
+  @Column({
+    type: 'enum',
+    enum: Exchange,
+    default: Exchange.MANUAL,
+  })
+  exchange_type: Exchange;
+
+  @Column('decimal', { precision: 8, scale: 2 })
+  sale_price: number;
+}
+
+export default Car;
